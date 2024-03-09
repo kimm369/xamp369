0a2ed0f30d86176b9fe197b3b3d1b50057442eb4
kimm369 <emmanuelsimiyu369@gmail.com>
2024-03-09 09:24:22 +0300

    initialcommit

---
+++ employee/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db import models
+
+class  Employee(models.Model):
+
+
+     eid =models.CharField(max_length=20)
+     ename = models.CharField(max_length=100)
+     email = models.EmailField()
+     econtact = models.CharField(max_length=15)
+     class Meta:
+        db_table="employee"
+# Create your models here.
